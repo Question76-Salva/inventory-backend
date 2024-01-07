82e8b05f383fb622ef99750c56ca07634ea27c77
Salvador Belloso Santos <50769284+Question76-Salva@users.noreply.github.com>
2024-01-07 13:54:55 +0100

    proyecto base de control de inventarios

---
+++ src/test/java/com/salva/P3CrudApiInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3CrudApiInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
